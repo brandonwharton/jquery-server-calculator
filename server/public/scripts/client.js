09605788c679be15e6c2202377156006328d307e
Brandon Wharton <whartonbm@gmail.com>
2021-05-23 15:20:52 -0500

    Changed history layout to a grid and made some minor function adjustments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -31,7 +31,7 @@ function handleReady() {
     // commands
     $('#equals').on('click', checkEquals);
     $('#clear').on('click', clearButton);
-    $('#deleteHistory').on('click', deleteRequest);
+    $('.historySec').on('click', '#deleteHistory', deleteRequest);
     $('#historySection').on('click', '.historyItem', repeatCalculation);
 
     // load previous calculations on page refresh
@@ -43,7 +43,6 @@ function handleReady() {
 function numberButton() {
     // target the button that was pushed and add it's value to numString
     numString += $(this).text();
-
     //show current numString in the display above the calculator
     $('#numDisplay').val(numString);
 }
@@ -51,7 +50,6 @@ function numberButton() {
 
 // handle all operand buttons
 function operatorButton() {
-
     // disallow operand if no numbers have been input and display error message
     if (numString === '') {
         errorMessage('Enter a number first');
@@ -85,14 +83,14 @@ function operatorButton() {
 // checks the currentCalculation object and string to ensure proper syntax before running a POST request
 function checkEquals() {
     // checks for a full expression entered, gives an error if not
-    //       makes sure two keys exist                     makes sure there are numbers after the operand
+    // makes sure two keys exist (a number and operand)      makes sure there are numbers after the operand
     if (Object.keys(currentCalculation).length === 2 && !isNaN(Number(numString.charAt(numString.length - 1)))) {
         // find the position of the operand in numString
         let operandPosition = numString.indexOf(currentCalculation.operation)
 
         // find the second number entered and add it's value to currenCalculation object
         let secondNumber = numString.substring(operandPosition + 1);
-        console.log(secondNumber);
+        // console.log(secondNumber);
         currentCalculation.number2 = secondNumber;
 
         // run equalsPost to POST data to server once object is checked and completed
@@ -115,7 +113,6 @@ function equalsPost() {
         // run functions to render DOM
         answerRequest();
         historyRequest();
-
         // reset everything using clearButton function
         clearButton();
     }).catch(function (error) {
@@ -127,12 +124,13 @@ function equalsPost() {
 
 function clearButton() {
     console.log('Clicked');
-    // clear input, reset both numString variable and currentCalculation object, and error messages
+    // clear input, reset both numString variable and currentCalculation object, and remove error messages
     $('input').val('');
     numString = '';
     currentCalculation = {};
     $('#errorSection').empty();
 }
+
 
 
 // GET requests
@@ -176,13 +174,17 @@ function historyRequest() {
             let num1 = calc.number1;
             let num2 = calc.number2;
             let op = calc.operation;
-
+            // append the history display to DOM, storing data for use during on-click recalculations
             el.append(`
                 <li class="historyItem" data-number1="${num1}" data-number2="${num2}" data-operation="${op}">
                     ${num1} ${op} ${num2} = ${ans}
                 </li>
             `);
         }) // end forEach
+        // append delete button at the bottom of the history list
+        el.append(`
+            <button id="deleteHistory" class="btn btn-danger">Delete History</button>
+        `)
     }).catch(function (error) {
         console.log(error);
         alert('Something went wrong, try again;');
@@ -192,13 +194,12 @@ function historyRequest() {
 
 function deleteRequest() {
     console.log('Requested a delete to /history');
-    
     $.ajax({
         url: '/history',
         type: 'DELETE',
         success: (function (response) {
             console.log(response);
-            // run answerRequest and historyRequest to update DOM
+            // refresh the Result display and run historyRequest to update DOM
             $('#resultSection').empty();
             $('#resultSection').append(`
                 <h2 class="result">Result: </h2>
@@ -211,6 +212,8 @@ function deleteRequest() {
     });
 }
 
+
+// populates an error message depending on the circumstances of the error
 function errorMessage(input) {
     // empty any previous error messages
     $('#errorSection').empty();
@@ -220,11 +223,11 @@ function errorMessage(input) {
     `);
 }
 
+
+// handles repeating previous calculations when clicking on history
 function repeatCalculation () {
-    console.log('Clicked!');
     // set currentCalculation to the data saved on the history li
     currentCalculation = $(this).data();
-
     // post the calculation again to repeat it
     equalsPost();
 }
